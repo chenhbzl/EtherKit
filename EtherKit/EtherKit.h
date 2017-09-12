e1db091612681db19121d289620c55a82462e6b3
Johan Sellström <johan.sellstrom@combient.com>
2017-09-12 15:43:28 +0200

    EtherKit

---
+++ EtherKit/EtherKit.h
@@ -0,0 +1,19 @@
+//
+//  EtherKit.h
+//  EtherKit
+//
+//  Created by Johan Sellström on 2017-09-12.
+//  Copyright © 2017 Ethers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EtherKit.
+FOUNDATION_EXPORT double EtherKitVersionNumber;
+
+//! Project version string for EtherKit.
+FOUNDATION_EXPORT const unsigned char EtherKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EtherKit/PublicHeader.h>
+
+
